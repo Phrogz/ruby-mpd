dc21198fbb807507f44bea593802eb938496af57
Gavin Kistner <gavin@phrogz.net>
2016-02-03 10:06:14 -0700

    command_list ignores results by default

--- lib/ruby-mpd/plugins/command_list.rb
+++ lib/ruby-mpd/plugins/command_list.rb
@@ -6,14 +6,17 @@ module MPD::Plugins
   module CommandList
     # Send multiple commands at once.
     #
+    # By default any response from the server is ignored (for speed).
+    # To get results, pass `results:true` as an option to the method.
+    #
     # Note that each supported command has no return value inside the block.
     # Instead, the block itself returns an array of results.
     #
     # @param [Hash] opts the options to create a message with.
-    # @option opts [Boolean] :results Set to false to ignore the server response (for speed).
-    # @return [Array] of command results (types may vary).
-    # @return [nil] with results:false, or if no commands have any results.
-    # 
+    # @option opts [Boolean] :results Set to true to process the server response (for speed).
+    # @return [nil] by default, or if no commands have any results.
+    # @return [Array] of command results if `results:true` is supplied (types may vary).
+    #
     # @example Simple batched control commands
     #   @mpd.command_list do
     #     stop
@@ -21,18 +24,20 @@ module MPD::Plugins
     #     save "shuffled"
     #   end
     #
+    # @example Adding songs to the queue, ignoring the response
+    #   @mpd.command_list do
+    #     my_songs.each do |song|
+    #       add(song)
+    #     end
+    #   end
+    #
     # @example Adding songs to the queue and getting the song ids
-    #   ids = @mpd.command_list do
+    #   ids = @mpd.command_list(results:true) do
     #     my_songs.each{ |song| addid(song) }
     #   end
     #
-    # @example Adding songs to the queue, ignoring the response
-    #   @mpd.command_list(results:false) do
-    #     my_songs.each{ |song| add(song) }
-    #   end
-    #
     # @see CommandList::Commands CommandList::Commands for a list of supported commands.
-    def command_list(opts={results:true},&commands)
+    def command_list(opts={},&commands)
       @mutex.synchronize do
         begin
           socket.puts "command_list_begin"
