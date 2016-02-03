f94e1451212df44e23ed527f5968b7b6881345e7
Gavin Kistner <gavin@phrogz.net>
2016-02-02 22:41:24 -0700

    Documentation for command_list

--- lib/ruby-mpd/plugins/command_list.rb
+++ lib/ruby-mpd/plugins/command_list.rb
@@ -4,15 +4,34 @@ module MPD::Plugins
 
   # Batch send multiple commands at once for speed.
   module CommandList
-    # Send multiple commands as a block.
-    # 
-    # For a list of supported commands, see CommandList::Commands
+    # Send multiple commands at once.
+    #
+    # Note that each supported command has no return value inside the block.
+    # Instead, the block itself returns an array of results.
     #
     # @param [Hash] opts the options to create a message with.
-    # @option opts [Boolean] :results Return results (`true`) or ignore them, for speed
-    # @return [Array] of command results.
-    # @return [nil] if no commands have any results.
-    # @see CommandList::Commands
+    # @option opts [Boolean] :results Set to false to ignore the server response (for speed).
+    # @return [Array] of command results (types may vary).
+    # @return [nil] with results:false, or if no commands have any results.
+    # 
+    # @example Simple batched control commands
+    #   @mpd.command_list do
+    #     stop
+    #     shuffle
+    #     save "shuffled"
+    #   end
+    #
+    # @example Adding songs to the queue and getting the song ids
+    #   ids = @mpd.command_list do
+    #     my_songs.each{ |song| addid(song) }
+    #   end
+    #
+    # @example Adding songs to the queue, ignoring the response
+    #   @mpd.command_list(results:false) do
+    #     my_songs.each{ |song| add(song) }
+    #   end
+    #
+    # @see CommandList::Commands CommandList::Commands for a list of supported commands.
     def command_list(opts={results:true},&commands)
       @mutex.synchronize do
         begin
@@ -41,7 +60,6 @@ module MPD::Plugins
 
     private
       def send_command(command,*args)
-        puts "PRIVATE #{command}"
         @mpd.send(:socket).puts @mpd.send(:convert_command, command, *args)
       end
   end
